eef7743de3702fa968bdbeb46fefbeedf6732cf9
lfflorian <lfflorian4@gmail.com>
2022-05-10 05:31:50 -0600

    configuration for docker container

--- GeoAPI.webapi/Services/GeoIPService.cs
+++ GeoAPI.webapi/Services/GeoIPService.cs
@@ -1,5 +1,6 @@
 ﻿using GeoAPI.Entities;
 using MaxMind.GeoIP2;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,17 @@ namespace GeoAPI.webapi.Services
 {
     public class GeoIPService : IGeoIPService
     {
+        private readonly IConfiguration _config;
+        public GeoIPService(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
         public IPLocation GetGeolocalization(string ip)
         {
             IPLocation iPLocation = new IPLocation();
             iPLocation.IP = ip;
 
-            using (var reader = new DatabaseReader(@"GeoLite2-Country.mmdb"))
+            using (var reader = new DatabaseReader(this._config["AppSettings:GeoDb"]))
             {
                 var response = reader.Country(iPLocation.IP);
 
