b677432d260844f94466d3e5ef391ac6341287b8
lfflorian <lfflorian4@gmail.com>
2022-05-09 19:31:00 -0600

    define the methods we use, and process for error handling

---
+++ GeoAPI.webapi/Controllers/GeolocationController.cs
@@ -0,0 +1,32 @@
+﻿using GeoAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoAPI.webapi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class GeolocationController : ControllerBase
+    {
+        public GeolocationController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok("Get its working");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            return Ok("Post its working");
+        }
+    }
+}
